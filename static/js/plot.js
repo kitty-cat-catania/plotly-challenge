63510c2473da2fdb92f42610fbb3a6aeac0ec195
toothFairyDragon <gabrielle.catania@gmail.com>
2021-05-20 09:49:17 -0700

    worked on creating dropdown menu

--- static/js/plot.js
+++ static/js/plot.js
@@ -7,4 +7,28 @@ d3.json("samples.json").then(function getNames(data) {
     return names;
 });
 
-//get test subject 'name' 
+
+//call function updateDash() when someone presses button
+d3.selectAll("#selDataset").on("change", optionChanged);
+
+//called whenever someone presses button for dropdownn menu
+function optionChanged() {
+    //select dropdown menu with d3
+    var ddMenu = d3.select("#selDataset");
+    //Assign value of ddmenu option to variable
+    var testID = ddMenu.property("value");
+
+    //initialize empty array for hell of it
+    var y = [];
+
+    //test to see if I can get options to display on the dropdown menu
+    if (testID=== "904") {
+        y = ["doggos", "no", "know"];
+        console.log(y);
+    }
+
+    else if (testID === "905") {
+        y = ["a", "b", "c"];
+        console.log(y);
+    }
+}
