caea32226578fb4133fa2fd9aa3885e8e44e3c9f
toothFairyDragon <gabrielle.catania@gmail.com>
2021-05-20 13:23:05 -0700

    Learned how to correctly append values to drop down menu

--- static/js/plot.js
+++ static/js/plot.js
@@ -1,34 +1,24 @@
-//getNames prints an the array of names to the console and 
-// also returns that same array
-
-d3.json("samples.json").then(function getNames(data) {
-    var names = data.names;
-    console.log(names);
-    return names;
-});
-
-
-//call function updateDash() when someone presses button
-d3.selectAll("#selDataset").on("change", optionChanged);
-
-//called whenever someone presses button for dropdownn menu
-function optionChanged() {
-    //select dropdown menu with d3
-    var ddMenu = d3.select("#selDataset");
-    //Assign value of ddmenu option to variable
-    var testID = ddMenu.property("value");
-
-    //initialize empty array for hell of it
-    var y = [];
-
-    //test to see if I can get options to display on the dropdown menu
-    if (testID=== "904") {
-        y = ["doggos", "no", "know"];
-        console.log(y);
-    }
-
-    else if (testID === "905") {
-        y = ["a", "b", "c"];
-        console.log(y);
-    }
+function getDropdown() {
+    d3.json("samples.json").then(function getNames(data) {
+        var names = data.names;
+        console.log(names);
+        return names;
+    });
+    
 }
+
+var testIds = getDropdown();
+console.log(testIds);
+
+var ddButton = d3.select("#selDataset");
+var idOption = ddButton.append("option");
+idOption.text("Please work");
+idOption.attr("value", "ugh")
+
+/*var ddOptions = testIds.map(function(name) {
+    var idOption = ddOptions.append("option").data(name)
+                            .enter()
+                            .append("option");
+    return idOption;
+});
+*/
